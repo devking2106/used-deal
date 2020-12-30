fe6ed285af3b0e925de9fdbbd4970160ea12bdd8
devking2106 <devking2106@gmail.com>
2020-12-30 18:16:12 +0900

    create used-deal

---
+++ src/main/java/me/devking2106/useddeal/UsedDealApplication.java
@@ -0,0 +1,13 @@
+package me.devking2106.useddeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedDealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/devking2106/useddeal/UsedDealApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devking2106.useddeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
